81eeb2516d680f27e7bb2b8052433d573c1350f4
its-mohd-kaif <mohdkaif@cedcoss.com>
2022-09-15 12:21:32 +0530

    Modification of Task 9 Done

--- src/task9.js
+++ src/task9.js
@@ -1,108 +1,95 @@
-var data1 = {
-  company: "samsung",
-  model: "galaxy",
-  memory: 64,
-  price: 15000,
-};
-var data2 = {
-  company: "nokia",
-  model: "S730",
-  memory: 128,
-  price: 22000,
-};
-var data3 = {
-  company: "xiaomi",
-  model: "note",
-  memory: 32,
-  price: 12000,
-};
-var data4 = {
-  company: "motoroala",
-  model: "g10",
-  memory: 32,
-  price: 15000,
-};
-var data5 = {
-  company: "apple",
-  model: "s12",
-  memory: 64,
-  price: 25000,
-};
+var data = [
+  {
+    Company: "Samsung",
+    Model: "Galaxy",
+    Memory: 64,
+    Price: 15000,
+    Rating: "",
+  },
+  {
+    Company: "Nokia",
+    Model: "S730",
+    Memory: 128,
+    Price: 22000,
+    Rating: "",
+  },
+  {
+    Company: "Xiaomi",
+    Model: "Note",
+    Memory: 32,
+    Price: 12000,
+    Rating: "",
+  },
+  {
+    Company: "Motoroala",
+    Model: "G10",
+    Memory: 32,
+    Price: 15000,
+    Rating: "",
+  },
+  {
+    Company: "Apple",
+    Model: "S12",
+    Memory: 64,
+    Price: 25000,
+    Rating: "",
+  },
+];
 
-var arr = [data1, data2, data3, data4, data5];
+function tbl() {
+  let table =
+    "<div><table border=1><tr><th id ='tdh'>Company</th><th id ='tdh'>Model</th> <th id ='tdh'>Memory(GB)</th><th id ='tdh'>Price(RS)</th><th id ='tdh'>Rating</th>";
+  document.getElementById("output").innerHTML = table;
+  data.forEach((element) => {
+    table +=
+      "<tr><td id='tds'>" +
+      element.Company +
+      "</td id='tds'><td id='tds'>" +
+      element.Model +
+      "</td> <td id='tds'>" +
+      element.Memory +
+      "</td> <td id='tds'>" +
+      element.Price +
+      "</td> <td id='tds'> " +
+      element.Rating;
+    ("</td></tr>");
+    document.getElementById("output").innerHTML = table;
+  });
 
-var a = [];
+  table += "</div></table>";
+}
 
 function add() {
   let tdata = document.getElementById("tdata").value;
   let trate = document.getElementById("trate").value;
 
-  if (tdata === "samsung") {
-    var list = {
-      company: arr[0].company,
-      model: arr[0].model,
-      memory: arr[0].memory,
-      price: arr[0].price,
-      rate: trate,
-    };
-    a.push(list);
-  } else if (tdata === "nokia") {
-    var list = {
-      company: arr[1].company,
-      model: arr[1].model,
-      memory: arr[1].memory,
-      price: arr[1].price,
-      rate: trate,
-    };
-    a.push(list);
-    console.log(arr);
-  } else if (tdata === "xiaomi") {
-    var list = {
-      company: arr[2].company,
-      model: arr[2].model,
-      memory: arr[2].memory,
-      price: arr[2].price,
-      rate: trate,
-    };
-    a.push(list);
-    console.log(arr);
-  } else if (tdata === "motoroala") {
-    var list = {
-      company: arr[3].company,
-      model: arr[3].model,
-      memory: arr[3].memory,
-      price: arr[3].price,
-      rate: trate,
-    };
-    a.push(list);
-    console.log(arr);
-  } else if (tdata === "apple") {
-    var list = {
-      company: arr[4].company,
-      model: arr[4].model,
-      memory: arr[4].memory,
-      price: arr[4].price,
-      rate: trate,
-    };
-    a.push(list);
-    console.log(arr);
+  if (tdata == "Samsung") {
+    data[0].Rating = trate;
+  } else if (tdata == "Nokia") {
+    data[1].Rating = trate;
+  } else if (tdata == "Xiaomi") {
+    data[2].Rating = trate;
+  } else if (tdata == "Motoroala") {
+    data[3].Rating = trate;
+  } else if (tdata == "Apple") {
+    data[4].Rating = trate;
   }
 
   let table =
-    "<div><table border=1><tr><th id ='tdh'>Company</th><th id ='tdh'>Model</th> <th id ='tdh'>Memory(GB)</th><th id ='tdh'>Price(RS)</th><th id ='tdh'>Rate</th>";
+    "<div><table border=1><tr><th id ='tdh'>Company</th><th id ='tdh'>Model</th> <th id ='tdh'>Memory(GB)</th><th id ='tdh'>Price(RS)</th><th id ='tdh'>Rating</th>";
   document.getElementById("res").innerHTML = table;
-  a.forEach((element) => {
+  data.forEach((element) => {
     table +=
       "<tr><td id='tds'>" +
-      element.company +
+      element.Company +
       "</td id='tds'><td id='tds'>" +
-      element.model +
+      element.Model +
       "</td> <td id='tds'>" +
-      element.memory +
+      element.Memory +
       "</td> <td id='tds'>" +
-      element.price +
+      element.Price +
       "</td> <td id='tds'> " +
-      element.rate;
+      element.Rating;
     ("</td></tr>");
     document.getElementById("res").innerHTML = table;
   });
