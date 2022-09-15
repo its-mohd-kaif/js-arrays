a4745c54c072353ff37a24240132024fdb0ea2f5
its-mohd-kaif <mohdkaif@cedcoss.com>
2022-09-15 12:34:02 +0530

    Modification of Task 10 is Done

--- src/task10.js
+++ src/task10.js
@@ -1,74 +1,59 @@
-var data1 = {
-  company: "samsung",
-  model: "galaxy",
-  memory: 64,
-  price: 15000,
-};
-var data2 = {
-  company: "nokia",
-  model: "S730",
-  memory: 128,
-  price: 22000,
-};
-var data3 = {
-  company: "xiaomi",
-  model: "note",
-  memory: 32,
-  price: 12000,
-};
-var data4 = {
-  company: "motoroala",
-  model: "g10",
-  memory: 32,
-  price: 15000,
-};
-var data5 = {
-  company: "apple",
-  model: "s12",
-  memory: 64,
-  price: 25000,
-};
-
-var arr = [data1, data2, data3, data4, data5];
-
-var a = [];
+var data = [
+  {
+    Company: "Samsung",
+    Model: "Galaxy",
+    Memory: 64,
+    Price: 15000,
+  },
+  {
+    Company: "Nokia",
+    Model: "S730",
+    Memory: 128,
+    Price: 22000,
+  },
+  {
+    Company: "Xiaomi",
+    Model: "Note",
+    Memory: 32,
+    Price: 12000,
+  },
+  {
+    Company: "Motoroala",
+    Model: "G10",
+    Memory: 32,
+    Price: 15000,
+  },
+  {
+    Company: "Apple",
+    Model: "S12",
+    Memory: 64,
+    Price: 25000,
+  },
+];
 
 function add() {
   var min = document.getElementById("min").value;
   var max = document.getElementById("max").value;
-  
-  if (data1.price >= min && data1.price <= max) {
-    a.push(data1);
-  }
-  if (data2.price >= min && data2.price <= max) {
-    a.push(data2);
-  }
-  if (data3.price >= min && data3.price <= max) {
-    a.push(data3);
-  }
-  if (data4.price >= min && data4.price <= max) {
-    a.push(data4);
-  }
-  if (data5.price >= min && data5.price <= max) {
-    a.push(data5);
-  }
 
   var table =
     "<div><table border=1><tr><th id ='tdh'>Company</th><th id ='tdh'>Model</th> <th id ='tdh'>Memory(GB)</th><th id ='tdh'>Price(RS)</th>";
   document.getElementById("res").innerHTML = table;
-  a.forEach((element) => {
-    table +=
-      "<tr><td id='tds'>" +
-      element.company +
-      "</td id='tds'><td id='tds'>" +
-      element.model +
-      "</td> <td id='tds'>" +
-      element.memory +
-      "</td> <td id='tds'>" +
-      element.price +
-      "</td> </tr>";
-    document.getElementById("res").innerHTML = table;
+  data.forEach((element) => {
+    if (element.Price >= min && element.Price <= max) {
+      table +=
+        "<tr><td id='tds'>" +
+        element.Company +
+        "</td id='tds'><td id='tds'>" +
+        element.Model +
+        "</td> <td id='tds'>" +
+        element.Memory +
+        "</td> <td id='tds'>" +
+        element.Price +
+        "</td> </tr>";
+      document.getElementById("res").innerHTML = table;
+    }
   });
 
   table += "</div></table>";
 }
+function display() {}
